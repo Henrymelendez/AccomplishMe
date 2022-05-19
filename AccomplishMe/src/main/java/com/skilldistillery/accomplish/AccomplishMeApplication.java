abc2bf82c018bb846fffcba1496890be3bdcc79f
Henrymelendez <henryomarm@gmail.com>
2022-05-19 16:20:08 -0400

    Create Boot MVC project, add JPA project Dependency

---
+++ AccomplishMe/src/main/java/com/skilldistillery/accomplish/AccomplishMeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.accomplish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccomplishMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccomplishMeApplication.class, args);
+	}
+
+}
---
+++ AccomplishMe/src/test/java/com/skilldistillery/accomplish/AccomplishMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.accomplish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccomplishMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
