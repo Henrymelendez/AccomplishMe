f62a136a68ddc0d8f7221891b0d20a09e92df7cf
Mason Keeney <mason.keeney68@gmail.com>
2022-05-21 07:01:32 -0600

    distinguished get and post methods for createUser.do

--- AccomplishMe/src/main/java/com/skilldistillery/accomplish/controllers/UserController.java
+++ AccomplishMe/src/main/java/com/skilldistillery/accomplish/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.accomplish.data.UserDAO;
@@ -27,7 +28,13 @@ public class UserController {
 		return view;
 	}
 	
-	@RequestMapping(path= "createUser.do")
+	@RequestMapping(path="createUser.do", method = RequestMethod.POST)
+	public String createUserPage() {
+		return "views/createUser";
+	}
+	
+	
+	@RequestMapping(path= "createUser.do", method = RequestMethod.POST)
 	public String createNewUser(User user, HttpSession session, RedirectAttributes redir) {
 		String view = "views/userHome";
 		userDAO.createaUser(user);
